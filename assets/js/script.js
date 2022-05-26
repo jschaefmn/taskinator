5e13092a330bed846c99f0589a15e16d317316b1
Jake Schaefbauer <jschaefmn@gmail.com>
2022-05-25 20:58:56 -0500

    finished 4.2.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var createFormHandler = function (event) {
 
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // package data up as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  // send it as an argument to createTaskEl
+  creatTaskEl(taskDataObj);
+}
+
+var creatTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -17,7 +28,7 @@ var createTaskHandler = function (event) {
   // give it a class name
   taskInfoEl.className = "task-info";
   // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
   listItemEl.appendChild(taskInfoEl);
 
@@ -25,4 +36,4 @@ var createTaskHandler = function (event) {
   tasksToDoEl.appendChild(listItemEl);
 }
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createFormHandler);
